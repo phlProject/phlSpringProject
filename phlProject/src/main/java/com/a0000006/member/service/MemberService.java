082a4cb4c74ca242618a3caa926fa97d52f6eee4
Lee <Lee@DESKTOP-7LD7O1E>
2017-07-22 12:31:19 +0900

    이름변경

---
+++ phlProject/src/main/java/com/a0000006/member/service/MemberService.java
@@ -0,0 +1,31 @@
+package com.a0000006.member.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.phl.common.CommandMap;
+
+public interface MemberService {
+
+	/* 로그인(ID,PW CHECK) */
+	List<Map<String, Object>> idPwCheck(Map<String, Object> map) throws Exception;
+	
+	/* 로그인 정보 */
+	List<Map<String, Object>> loginInfo(Map<String, Object> map) throws Exception;
+	
+	/* 마이페이지 */
+	List<CommandMap> memMyPage(Map<String, Object> map) throws Exception;
+	
+	/* 회원 등록 */
+	String insertMemRegist(Map<String, Object> map) throws Exception;
+
+	/* 마이페이지 수정 */
+	String updateMemRegist(Map<String, Object> map) throws Exception;
+	
+	/* 회원탈퇴 */
+	String deleteMemRegist(Map<String, Object> map) throws Exception;
+	
+	/* 아이디 중복체크 */
+	public int idDupChk(Map<String, Object> map) throws Exception;
+	
+}
---
+++ phlProject/src/main/java/com/a0000006/member/service/MemberServiceImpl.java
@@ -0,0 +1,82 @@
+package com.a0000006.member.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.a0000006.member.dao.MemberDAO;
+import com.phl.common.CommandMap;
+
+@Service("memberService")
+public class MemberServiceImpl implements MemberService {
+	
+	Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name="memberDAO")
+	private MemberDAO memberDAO;
+
+	/* 로그인(ID,PW CHECK) */
+	@Override
+    public List<Map<String, Object>> idPwCheck(Map<String, Object> map) throws Exception {
+        return memberDAO.idPwCheck(map);
+    }
+	
+	/* 로그인 정보 */
+	@Override
+    public List<Map<String, Object>> loginInfo(Map<String, Object> map) throws Exception {
+        return memberDAO.loginInfo(map);
+    }
+	
+	/* 마이페이지  */
+	@Override
+    public List<CommandMap> memMyPage(Map<String, Object> map) throws Exception {
+        return memberDAO.memMyPage(map);
+    }
+	
+	/* 회원 등록 */
+	@Override
+	public String insertMemRegist(Map<String, Object> map) throws Exception {
+		try {
+			memberDAO.insertMemRegist(map);
+			return "success";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "fail";
+		}
+	}
+	
+	/* 마이페이지 수정 */
+	@Override
+	public String updateMemRegist(Map<String, Object> map) throws Exception {
+		try {
+			memberDAO.updateMemRegist(map);
+			return "success";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "fail";
+		}
+	}
+	
+	/* 회원탈퇴 */
+	@Override
+	public String deleteMemRegist(Map<String, Object> map) throws Exception {
+		try {
+			memberDAO.deleteMemRegist(map);
+			return "success";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "fail";
+		}
+	}
+	
+	/* 아이디 중복체크 */
+	@Override
+    public int idDupChk(Map<String, Object> map) throws Exception {
+        return memberDAO.idDupChk(map);
+    }
+	
+}
